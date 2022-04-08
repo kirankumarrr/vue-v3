0544e7bb3d2d1b6422307c7e2a400163165be930
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-04-08 21:54:57 +0530

    [vue-composition-api-basics] - options to composition setup

---
+++ vue-composition-api-basics-v3/src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <!-- this allow us to display views -->
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ vue-composition-api-basics-v3/src/views/HomeView.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="home">
+    <div>
+      <button class="btn" @click="decrement">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button class="btn" @click="increment">+</button>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const decrement = () => {
+  counter.value--;
+};
+const increment = () => {
+  counter.value++;
+};
+</script>
+
+<!--  this is before v3.2 -> without setup 
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const decrement = () => {
+      counter.value--;
+    };
+    const increment = () => {
+      counter.value++;
+    };
+
+    return {
+      counter,
+      decrement,
+      increment,
+    };
+  },
+};
+</script>
+ -->
+
+
+<!--
+  this is optional api
+ <script>
+ export default {
+   data() {
+     return {
+       counter: 0,
+     };
+   },
+   methods: {
+     decrement() {
+       this.counter--;
+     },
+     increment() {
+       this.counter++;
+     },
+   },
+ };
+
+</script>
+ -->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
