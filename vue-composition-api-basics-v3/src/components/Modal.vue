5e43b97f7e6f38c0935bfa65264d2b8aa1bc5079
Vanga Kiran Kumar Reddy <kirankumarrrreddy@gmail.com>
2022-04-10 13:24:26 +0530

    [vue-composition-api-basics]  Child Components, Props & Emits part 1

---
+++ vue-composition-api-basics-v3/src/components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>
+                <slot name="title" />
+            </h1>
+            <h2>{{ subTitle }}</h2>
+            <slot />
+            <!-- <pre>{{ $slots.title() }}</pre> -->
+            <!-- <button @click="$emit('hideModal')">Hide Modal</button> -->
+            <button @click="handleBtnClick">Hide Modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+import { useSlots } from 'vue';
+
+const slots = useSlots()
+console.log('slots', slots.title())
+
+
+/***
+ * Props 
+ */
+const props = defineProps({
+    subTitle: {
+        type: String,
+        default: 'no subtitle sepecified'
+    }
+})
+
+/***
+ * Emits 
+ */
+const emit = defineEmits(['hideModal'])
+
+// Programatically
+const handleBtnClick = () => {
+    emit('hideModal')
+}
+
+</script>
+
+<style>
+.modal {
+    background: #00bd6e;
+    padding: 10px;
+    color: black;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- vue-composition-api-basics-v3/src/router/index.js
+++ vue-composition-api-basics-v3/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PostView from '../views/PostView.vue'
+import ModalsView from '../views/ModalsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +18,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/post',
@@ -23,7 +26,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PostView.vue')
+      component: PostView
     },
     {
       path: '/modals',
@@ -31,7 +34,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ModalsView.vue')
+      component: ModalsView
     },
     {
       path: '/postDetail/:id',
